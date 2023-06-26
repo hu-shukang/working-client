8ed473686524e2d39c2a3d11e56249cbfb80c464
HU SHUKANG <hushukang@gmail.com>
2023-06-26 20:56:57 +0900

    change eslint setting

--- pages/attendance/index.vue
+++ pages/attendance/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <AttendanceTable :list="list" />
+  <div>
+    <div>{{ result }}</div>
+    <div>{{ pending }}</div>
+    <div>{{ error }}</div>
+    <AttendanceTable :list="list" />
+  </div>
 </template>
 
 <script setup lang="ts">
   import { attendanceList } from '~/types/attendance.type';
 
   const list = attendanceList();
+  const { data: result, pending, error } = await useFetch('https://holidays-jp.github.io/api/v1/2015/date.json');
 </script>
