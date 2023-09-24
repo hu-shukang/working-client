9433b187d8a94e1254dee5f65b946242dcd5cff4
HU SHUKANG <hushukang@gmail.com>
2023-09-24 21:55:30 +0900

    add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="alert alert-primary" role="alert">
+  <div
+    class="alert alert-primary"
+    role="alert"
+  >
     A simple primary alert—check it out!
   </div>
   <HelloWorld msg="hello" />
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
