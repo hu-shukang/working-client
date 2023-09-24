10ea0c710d8dde440fd9ad17041a855a4e15afb9
HU SHUKANG <hushukang@gmail.com>
2023-09-24 21:28:55 +0900

    init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+  </div>
+  <HelloWorld msg="hello" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
